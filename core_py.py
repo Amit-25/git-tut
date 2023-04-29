6cada1c5ceec5b7f08f20f71acdb7bc2915dc32f
Amit-25 <singhamit25may@gmail.com>
2023-04-29 13:53:35 +0530

    Initital Commit

---
+++ core_py.py
@@ -0,0 +1,4 @@
+import math
+val = -11
+val +=1
+print(round(1.51))
---
+++ p1.py
@@ -0,0 +1,21 @@
+from time import sleep
+from threading import Thread
+
+videos = ['DSA', 'OS', 'Networking']
+
+print('Track the git changes')
+
+class MyThreadClass(Thread):
+    
+    def __init__(self,value):
+        self.value = value
+        Thread.__init__(self)
+    
+    def run(self):
+        for vid in videos:
+            print(f"{vid} is uploading..,")
+            sleep(1)
+            print(f"{vid} uploaded successfully.")
+
+t1 = MyThreadClass(10)
+t1.start()
